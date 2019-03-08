d60ea9c056d89a0d791f1bb5355ac22b2f4d14bc
xiaohu <1205150638@qq.com>
2019-03-08 18:09:46 +0800

    fix bug

---
+++ blog/app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    # ?numbers[]=1&numbers[]=2&numbers[]=3 pass array
+    # user[id]=1&user[name]=Nathan     pass object
+    # users[][id]=1&users[][name]=Nathan&users[][id]=2&users[][name]=Emma   Complex Objects
+    logger.info "[param]#{params[:numbers]}"
+    logger.info "[param]#{params[:user]}"
+    logger.info "[param]#{params[:users][0]}"
+  end
+
+  def new
+
+  end
+
+  def create
+    # 观察 params 传递的 param 是嵌套的
+    logger.info "[param]#{params}"
+  end
+
+end
