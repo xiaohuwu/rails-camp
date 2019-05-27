fde5c668b4d609a8b705c16d649c5ccafe9efe14
xiaohu <1205150638@qq.com>
2019-05-27 14:58:21 +0800

    fix bug

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   mount_uploader :pic, AvatarUploader
   validates :name, presence: true, length: {minimum: 2, maximum: 20}, uniqueness: true
-  validates :email, presence: true, email: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6, maximum: 16}
   has_many :posts
 
