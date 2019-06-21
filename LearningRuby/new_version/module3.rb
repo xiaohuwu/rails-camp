24d06f7b257dcada01cfe8aac6d6291934b6170e
xiaohu <1205150638@qq.com>
2019-06-21 18:15:06 +0800

    fi xbug

--- LearningRuby/new_version/module3.rb
+++ LearningRuby/new_version/module3.rb
@@ -2,14 +2,16 @@ class Test
   def Test.say_hello
     puts "hello from #{self.name}"
   end
+
   say_hello
 end
 
 class Test1
-  @cls_var = 123
+  @@cls_var = 123
   attr_reader :cls_var
+
   def Test1.inc
-    @cls_var+=1
+    @@cls_var += 1
   end
 end
 
