28d94514fb096350507499a1f588d384f8d2f502
xiaohu <1205150638@qq.com>
2022-10-27 16:24:20 +0800

    fx

--- railsDemo/app/models/user.rb
+++ railsDemo/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :orders
+
   # has_secure_password
 
 
@@ -9,26 +12,26 @@ class User < ApplicationRecord
   # before_create {
   #   generate_token(:auth_token)
   # }
-
-  has_and_belongs_to_many :offical_accounts,:join_table => "user_offical_accounts"
-  has_many :moments
-
-  has_one :location, :as => :loc
-  has_and_belongs_to_many :followers, :join_table => "user_follow_users",:class_name => "User",:foreign_key => "followee_id",
-                          association_foreign_key: "follower_id"
- 
-
-
-  def generate_token(column)
-    begin
-      self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
-  end
-
-  def avatar
-    gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=512&d=retro"
-  end
+  #
+  # has_and_belongs_to_many :offical_accounts,:join_table => "user_offical_accounts"
+  # has_many :moments
+  #
+  # has_one :location, :as => :loc
+  # has_and_belongs_to_many :followers, :join_table => "user_follow_users",:class_name => "User",:foreign_key => "followee_id",
+  #                         association_foreign_key: "follower_id"
+  #
+  #
+  #
+  # def generate_token(column)
+  #   begin
+  #     self[column] = SecureRandom.urlsafe_base64
+  #   end while User.exists?(column => self[column])
+  # end
+  #
+  # def avatar
+  #   gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
+  #   "http://gravatar.com/avatar/#{gravatar_id}.png?s=512&d=retro"
+  # end
 
 
 end
